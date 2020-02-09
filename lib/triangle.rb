191317ecc745fde46162e4e9a21f09fc0a5c06e1
Chris Wade <swade781977@yahoo.com>
2020-02-09 01:35:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,18 +6,18 @@ class Triangle
   
   attr_accessor :side1, :side2, :side3
   
-  @@sides = []
+ 
   
   def initialize(side1, side2, side3)
     @side1 = side1
-    @@sides << side1
     @side2 = side2
-    @@sides << side2
     @side3 = side3
-    @@sides << side3
+    
   end
   
   def kind
+    @sides = []
+    self.each{|side| sides << side}
     sides.each |side|
       if 0 >= side
         begin
