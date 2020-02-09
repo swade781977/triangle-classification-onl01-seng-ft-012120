932a8f69a333c83b2b8f0cded6b45c9315cdcb41
Chris Wade <swade781977@yahoo.com>
2020-02-09 03:14:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+    def message
+      "The current side lengths do not currently create a triangle. Please make sure side lengths are \n 1/) Greater than 0 \n 2/) That the sum of any two sides is greater than the third side."
+    end
+  end
+  
+  attr_accessor :a, :b, :c
+  
+ 
+  @@sides = []
+  def initialize(a,b,c)
+    @a = a 
+    @b = b 
+    @c = c
+    @@sides = [a,b,c]
+  end
+  
+  def kind
+    if @@sides.max < @@sides.sum/2.0 && @@sides.each{|side| side > 0}
+      if a == b && b == c 
+        :equilateral
+      elsif a == b && b != c 
+        :isosceles
+      elsif b == c && c != a
+        :isosceles
+      elsif a == c && c != b 
+        :isosceles
+      elsif a != b && b != c 
+        :scalene
+      end
+    else
+        raise TriangleError
+    end
+  end
 end
+
