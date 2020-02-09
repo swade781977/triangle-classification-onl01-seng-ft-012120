d29a146b901c4d9def28a5ba9a0792ba6d29272d
Chris Wade <swade781977@yahoo.com>
2020-02-09 02:19:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,36 +13,14 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    
   end
   
-  def kind
-    if side1 <= 0 && side2 <= 0 && side3 <= 0
-        begin
-          raise TriangleError
-          rescue TriangleError => error 
-            puts error.message
-        end
-    elsif side1 + side2 <= sides3 || side1 + side3 <= side2 || side2 + side3 <= side1 
-        begin
-          raise TriangleError
-          rescue TriangleError => error 
-            puts error.message
-        end
-    elsif
-      if side1 == side2 && side2 == side3 
-        :equilateral
-      elsif side1 == side2 && side2 != side3 
-        :isosceles
-      elsif side2 == side3 && side3 != side1
-        :isosceles
-      elsif side1 == side3 && side3 != side2
-        :isosceles
-      elsif side1 != side2 && side2 != sides3
-        :scalene
-      end
-    end
-  end
+    def is_triangle?(a,b,c)
+  [a,b,c].max < [a,b,c].inject(:+)/2.0
 end
+  
+  def kind(a,b,c)
+    
+    
 end
 
